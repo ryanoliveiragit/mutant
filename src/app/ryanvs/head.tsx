9fbc3a2d81b6d1f9f6ddfff5fd6dafbdc9e825c2
Mikael Marceniuk <97217108+ryanoliveiragit@users.noreply.github.com>
2023-08-09 19:10:03 -0300

    update

--- src/app/ryanvs/head.tsx
+++
@@ -1,5 +0,0 @@
-export default function RyanvsHead() {
-    return <>
-        <title>ryanvs</title>
-    </>
-}
--- src/app/wfoods/head.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function WfoodsHead() {
-  return (
-    <head>
-      <title>wfoods - cliente</title>
-    </head>
-  );
-}
